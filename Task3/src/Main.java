636682bc6db117b462ee37743dc8531ad0005609
prisakura <leon.prisacaru@gmail.com>
2024-07-31 06:27:38 +0300

    Task 3 done

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final String VALID_REGEX = "((([А-я]+)|([А-я-]+))\\s){2}(([А-я]+)|([А-я-]+))";
+
     public static void main(String[] args) {
-        //todo ФИО
+        System.out.print("Введите Ф.И.О: ");
+        String input = new Scanner(System.in).nextLine();
+        input=input.trim();
+        if (input.matches(VALID_REGEX))
+            System.out.println(input.replace(" ","\n"));
     }
 }
